844219d72480212dcf4e2e4fc6e0d1a742f0fb04
monkeytechgit <monkeytechmx@gmail.com>
2025-06-30 12:20:47 -0700

    Delete pagina_web directory

--- pagina_web/supabase-config.js
+++
@@ -1,9 +0,0 @@
-// Configuración de Supabase
-const SUPABASE_CONFIG = {
-    url: 'https://nxzyodpetbfhgishwbrx.supabase.co',
-
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54enlvZHBldGJmaGdpc2h3YnJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3NzExNDMsImV4cCI6MjA2NTM0NzE0M30.mjLMUonFEfd8Q7FPfmLnzdE5c61HcZP3mLm-om8vMpc'
-};
-
-// No modifiques esta línea
-window.SUPABASE_CONFIG = SUPABASE_CONFIG; 
